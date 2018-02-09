1be35f035f7b32bae58a98c187c39d68f1dad1fd
ybelikov <yuribelikov@gmail.com>
2018-02-09 17:56:51 +0300

    dialogs and menus removed

--- src/bel/tetris/game/Runner.java
+++ src/bel/tetris/game/Runner.java
@@ -1,8 +1,6 @@
 package bel.tetris.game;
 
 import bel.tetris.container.ImageContainer;
-import bel.tetris.dialog.CNewGameBox;
-import bel.tetris.dialog.CYesNoBox;
 import bel.tetris.event.CEvent;
 import bel.tetris.event.CEventListener;
 import bel.tetris.event.IEventReceiver;
@@ -20,10 +18,7 @@ public class Runner extends Frame implements IEventReceiver
   private CEventListener eventListener = null;
   private MenuBar MB = null;
   private Menu gameMenu = null;
-  private MenuItem newGameMI = null, scoresMI = null, optionsMI = null, quitMI = null;
-
-  public CYesNoBox yesNoBox = null;
-  public CNewGameBox newGameBox = null;
+  private MenuItem newGameMI = null;
 
 
   public void destroy()
@@ -52,8 +47,8 @@ public class Runner extends Frame implements IEventReceiver
         String keyKode = keyEvt.getKeyText(keyEvt.getKeyCode());
 
         for (int n = 0; n < options.keys.length; n++)
-            if (options.keys[n].equals(keyKode))
-              cup.processAction(options.actions[n]);
+          if (options.keys[n].equals(keyKode))
+            cup.processAction(options.actions[n]);
 
         if (keyKode.equals("Pause"))
           cup.processAction("Pause");
@@ -63,19 +58,11 @@ public class Runner extends Frame implements IEventReceiver
       if (evt.getName().equals("ActionPerformed") && evt.getSourceName().equals("NewGame"))
         cup.processAction("NewGame");
 
-      if ((evt.getName().equals("ActionPerformed") && evt.getSourceName().equals("Quit")) ||
-              (evt.getName().equals("WindowClosing")))
+      if (evt.getName().equals("WindowClosing"))
       {
         cup.processAction("Quit");
-        if (yesNoBox.showBox("�����������", "�� �������, ��� ������ ����� �� ��������� ?"))
-        {
-          cup.finishGame();
-          destroy();
-        }
-        else
-        {
-          cup.processAction("No");
-        }
+        cup.finishGame();
+        destroy();
       }
     }
     catch (Exception e)
@@ -90,7 +77,7 @@ public class Runner extends Frame implements IEventReceiver
     {
 //Log.LogEnabled=true;
       setBackground(Color.black);
-      setTitle("������");
+      setTitle("Tetris");
       setLayout(null);
       setResizable(false);
       ImageContainer.load(this);
@@ -104,52 +91,30 @@ public class Runner extends Frame implements IEventReceiver
       Font f = new Font("Dialog", 0, 12);
       MB = new MenuBar();
       setMenuBar(MB);
-      gameMenu = new Menu("����");
+      gameMenu = new Menu("Game");
       MB.add(gameMenu);
       gameMenu.setFont(f);
 
-      newGameMI = new MenuItem("����� ����");
+      newGameMI = new MenuItem("New Game");
       newGameMI.setName("NewGame");
       gameMenu.add(newGameMI);
       newGameMI.setFont(f);
       newGameMI.addActionListener(eventListener);
       gameMenu.addSeparator();
 
-      scoresMI = new MenuItem("��� �����");
-      scoresMI.setName("scores");
-      gameMenu.add(scoresMI);
-      scoresMI.setFont(f);
-      scoresMI.addActionListener(eventListener);
-      gameMenu.addSeparator();
-
-      optionsMI = new MenuItem("���������");
-      optionsMI.setName("options");
-      gameMenu.add(optionsMI);
-      optionsMI.setFont(f);
-      optionsMI.addActionListener(eventListener);
-      gameMenu.addSeparator();
-
-      quitMI = new MenuItem("�����");
-      quitMI.setName("Quit");
-      gameMenu.add(quitMI);
-      quitMI.setFont(f);
-      quitMI.addActionListener(eventListener);
-
-      yesNoBox = new CYesNoBox();
-      newGameBox = new CNewGameBox();
-
       options = new COptions();
       cup = new CCup(this, 1);
     }
     catch (Exception e)
     {
-     e.printStackTrace();
+      e.printStackTrace();
     }
   }
 
   public static void main(String[] args)
   {
-    Runner app = new Runner();;
+    Runner app = new Runner();
+    ;
 
     try
     {
@@ -166,8 +131,11 @@ public class Runner extends Frame implements IEventReceiver
   {
     try
     {
-      Hashtable data = newGameBox.showBox();
-      if (data == null) return;
+      Hashtable data = new Hashtable();
+      data.put("PlayersQ", "1");
+      data.put("Player1Name", "PlayerName");
+      data.put("Speed", "2");
+      data.put("IsNextFigureShowed", "true");
 
       cup.finishGame();
       getGraphics().clearRect(0, 0, getSize().width, getSize().height);
